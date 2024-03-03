9ec928cd6af98f900b6122544f2972673eb0c12e
DmitryPotorochin <79530005262@yandex.ru>
2024-03-03 22:52:42 +0500

    исправленный вариант

--- js/main.js
+++ js/main.js
@@ -1,49 +1,3 @@
-function createObject() {
-  return {
-    id: randomId(),
-    url: 'photos/' + randomUrl() + '.jpg',
-    description: getRandomElement(DESCRIPTION),
-    likes: randomLikes,
-    comments: {
-      id: randomCommentsItd,
-      avatar: 'img/avatar-' + randomCommentsAvatar + '.svg',
-      message: getRandomElement(MESSAGE),
-      name: getRandomElement(NAME),
-    }
-  };
-}
-
-const randomId = createRandomIdFromRangeGenerator(1, 25);
-const randomUrl = createRandomIdFromRangeGenerator(1, 25);
-const randomLikes = getRandomInteger(15, 200);
-const randomCommentsItd = getRandomInteger(1, 3);
-const randomCommentsAvatar = getRandomInteger(1, 6);
-
-const DESCRIPTION = [
-  'Rhinoceros',
-  'Guinea Fowl',
-  'Squid',
-  'Deer',
-  'Dormouse'
-];
-
-const MESSAGE = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-const NAME = [
-  'Вася',
-  'Петя',
-  'Дима',
-  'Илья',
-  'Рома'
-];
-
 function getRandomElement(elements) {
   return elements[getRandomInteger(0, elements.length - 1)];
 }
@@ -55,24 +9,55 @@ function getRandomInteger(min, max) {
   return Math.floor(result);
 }
 
-function createRandomIdFromRangeGenerator(min, max) {
-  const previousValues = [];
+const descriptions = [
+  'Rhinoceros',
+  'Guinea Fowl',
+  'Squid',
+  'Deer',
+  'Dormouse'
+];
 
-  return function () {
-    let currentValue = getRandomInteger(min, max);
-    if (previousValues.length >= (max - min + 1)) {
-      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const names = [
+  'Вася',
+  'Петя',
+  'Дима',
+  'Илья',
+  'Рома'
+];
+
+let commentId = 1;
+
+function createComment() {
+  return {
+    id: commentId++,
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: getRandomElement(messages),
+    name: getRandomElement(names),
   };
 }
 
-const simiralObjects = Array.from({length: 25}, createObject);
+function createOffer(_, index) {
+  return {
+    id: index + 1,
+    url: `photos/${ index + 1 }.jpg`,
+    description: getRandomElement(descriptions),
+    likes: getRandomInteger(15, 200),
+    comments: Array.from({ length: getRandomInteger(0, 10) }, createComment),
+  };
+}
 
-console.log(simiralObjects);
+function createOffers(length) {
+  return Array.from({ length }, createOffer);
+}
 
+// eslint-disable-next-line no-console
+console.log(createOffers(25));
